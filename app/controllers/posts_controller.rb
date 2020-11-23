69c0d7ebaa9698bc1ff4201102a72b655087517a
sonoda <sonoponsyougo@gmail.com>
2020-11-23 21:08:06 +0900

    投稿の編集・削除

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,6 +12,20 @@ class PostsController < ApplicationController
     Post.create(post_params)
   end
 
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+  end
+
   private
 
   def post_params
