fce255323247c4b999d83e1827c2822ad79724d6
sonoda <sonoponsyougo@gmail.com>
2020-11-23 17:57:47 +0900

    新規投稿機能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,20 @@
 class PostsController < ApplicationController
-  before_action :
 
   def index
     @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    Post.create(post_params)
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
