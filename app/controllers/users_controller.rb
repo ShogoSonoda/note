c3d7558092441c94870c4f707dfd0c004e870d40
sonoda <sonoponsyougo@gmail.com>
2020-11-25 06:40:14 +0900

    名前と自己紹介文の編集ができる

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,23 @@
 class UsersController < ApplicationController
   def show
+    
     @nickname = current_user.nickname
     @posts = current_user.posts
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to action: :show
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:nickname, :email, :introduction)
   end
 end
