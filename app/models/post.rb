3ba57741bb1fad0061dc4f67febb603e90332ea9
sonoda <sonoponsyougo@gmail.com>
2020-11-28 06:44:00 +0900

    searchメソッドの定義

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,17 @@
 class Post < ApplicationRecord
   belongs_to :user
 
-    validates :title, presence: true
-    validates :text, presence: true
-    validates :user_id, presence:true
+  with_options presence: true do
+    validates :title
+    validates :text
+    validates :user_id
+  end
+
+  def self.search(search)
+    if search != ""
+      Post.where('text LIKE(?)', "%#{search}%")
+    else
+      Post.all
+    end
+  end
 end
