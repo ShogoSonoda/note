06e83a5296a2b77a2a05dc08911133e2fa86a142
sonoda <sonoponsyougo@gmail.com>
2020-12-13 09:33:13 +0900

    a

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,20 @@
 class UsersController < ApplicationController
+
   def show
     @user = User.find(params[:id])
-    @posts = current_user.posts
+    @posts = @user.posts
     @like_posts = @user.like_posts
   end
 
-  def edit
-    @user = User.find(params[:id])
-  end
+  # def edit
+  #   @user = User.find(params[:id])
+  # end
 
-  def update
-    user = User.find(params[:id])
-    user.update(user_params)
-    redirect_to action: :show
-  end
+  # def update
+  #   user = User.find(params[:id])
+  #   user.update_attributes(user_params)
+  #   redirect_to action: :show
+  # end
 
   def follows
     user = User.find(params[:id])
